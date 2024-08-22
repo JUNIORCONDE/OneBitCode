bd5ecfbf99b53f40ea1f5ed31da4d54d5e69a631
juniorconde <rubenscondejr@uol.com.br>
2024-08-21 22:06:49 -0300

    up

---
+++ metodos.rb
@@ -0,0 +1,12 @@
+# Aula Onebitconde, de Metodos e gems, encapsular codigos, abaixo alguns exemplos
+
+
+def hello (name)
+  puts "Ola #{name}"
+end
+
+
+puts "Como é seu nome?"
+name = gets.chomp
+
+puts "hello #{name}"
